da589a46b7b3fcea3ab2bf161fee416d314d1403
KatharinaViktoria <khoebel@mit.edu>
2023-10-05 15:07:03 -0400

    project utilities, currently contains functions to generate project paths

---
+++ utils.py
@@ -0,0 +1,62 @@
+import os 
+import pandas as pd
+
+
+def read_prediction_df(dataset:str,
+                       train_split:str, 
+                       model:str, 
+                       split: str, 
+                       path_template = 'data/splits/{0}/{1}/pathology/prediction_dfs/{2}/pred_{0}-{3}_df.csv',
+                       project_root= '/lotterlab/users/khoebel/xray_generalization'
+                      ):
+    
+    # reads in the prediction for the specified dataset and model 
+    # dataset: prediction dataset ('mmc', 'cxp')
+    # train_split: percentage of data the model has been trained on 
+    # model: name of the model used to generate the predictions 
+    # split: dataset split ('train', 'val','test')
+    
+    path = os.path.join(project_root,path_template.format(dataset, train_split, model, split))
+    
+    return pd.read_csv(path)
+
+
+def read_dataset_df(dataset:str,
+                    train_split:str,
+                    file_name_modifier: str,
+                    split: str,
+                    path_template = 'data/splits/{0}/{1}/pathology/{2}{3}.csv',
+                    project_root= '/lotterlab/users/khoebel/xray_generalization'
+                   ):
+    # reads in the dataset spreadsheet for the specified dataset and split 
+    # dataset: prediction dataset ('mmc', 'cxp')
+    # train_split: percentage of data the model has been trained on 
+    # split: dataset split ('train', 'val','test')
+    # file_name_modifier: to comply with naming conventions between cxp and mmc ('', 'meta_')
+     
+    path = os.path.join(project_root,path_template.format(dataset, train_split, file_name_modifier, split))
+    
+    return pd.read_csv(path)
+
+
+def save_score_label_df(df, 
+                  dataset:str,
+                  train_split:str, 
+                  split: str,
+                  path_sign: str,
+                  file_name_modifier: str,
+                  path_template = 'data/splits/{0}/{1}/{2}/{3}{4}.csv',
+                  project_root= '/lotterlab/users/khoebel/xray_generalization'
+                 ):
+    
+    # save score model labels
+    # dataset: prediction dataset ('mmc', 'cxp')
+    # train_split: percentage of data the model has been trained on 
+    # model: name of the model used to generate the predictions 
+    # split: dataset split ('train', 'val','test')
+    # path_sign: label for which the score labels have been generated 
+    # file_name_modifier: to comply with naming conventions between cxp and mmc ('', 'meta_')
+    
+    path = os.path.join(project_root, path_template.format(dataset, train_split, path_sign, file_name_modifier, split))
+    df.to_csv(path)
+        
